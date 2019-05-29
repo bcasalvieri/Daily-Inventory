6ec948d4606d82619bda7314ecbb1bda4d4a8e22
Brad Cas <bmcasalvieri@gmail.com>
2019-05-28 21:58:07 -0400

    created models

---
+++ models/Entry.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+
+const { Schema } = mongoose;
+
+const EntrySchema = new Schema({
+  created: new Date(),
+  resentful: {
+    type: Boolean,
+    required: true
+  },
+  resentfulNote: String,
+  selfish: {
+    type: Boolean,
+    required: true
+  },
+  selfishNote: String,
+  dishonest: {
+    type: Boolean,
+    required: true
+  },
+  dishonestNote: String,
+  afraid: {
+    type: Boolean,
+    required: true
+  },
+  afraidNote: String,
+  patient: {
+    type: Boolean,
+    required: true
+  },
+  patientNote: String,
+  apology: {
+    type: Boolean,
+    required: true
+  },
+  apologyNote: String,
+  prayer: {
+    type: Boolean,
+    required: true
+  },
+  prayerNote: String,
+  sobriety: {
+    type: Boolean,
+    required: true
+  },
+  sobrietyNote: String,
+});
+
+module.exports = mongoose.model("entry", EntrySchema);
---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const saltRounds = 10;
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\../, "Please use a valide email address!"]
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre('save', function createPassword(next) {
+  if (this.isNew || this.isModified('password')) {
+    const document = this;
+    bcrypt.hash(this.password, saltRounds, (err, hashedPassword) => {
+      if (err) {
+        next(err);
+      } else {
+        document.password = hashedPassword;
+        next();
+      };
+    });
+  };
+});
+
+UserSchema.methods.isCorrectPassword = function isCorrectPassword(password) {
+  const document = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, document.password, function compareCallback(err, same) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(same);
+      };
+    });
+  });
+};
+
+module.exports = mongoose.model("user", UserSchema);
