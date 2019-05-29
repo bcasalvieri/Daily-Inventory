656512a234f9cdcca510edb60f3936a4f99db7a6
Brad Cas <bmcasalvieri@gmail.com>
2019-05-28 23:03:25 -0400

    removed bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-const saltRounds = 10;
 
 const { Schema } = mongoose;
 
@@ -22,33 +19,5 @@ const UserSchema = new Schema({
     required: true
   }
 });
-
-UserSchema.pre('save', function createPassword(next) {
-  if (this.isNew || this.isModified('password')) {
-    const document = this;
-    bcrypt.hash(this.password, saltRounds, (err, hashedPassword) => {
-      if (err) {
-        next(err);
-      } else {
-        document.password = hashedPassword;
-        next();
-      };
-    });
-  };
-});
-
-UserSchema.methods.isCorrectPassword = function isCorrectPassword(password) {
-  const document = this;
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, document.password, function compareCallback(err, same) {
-      if (err) {
-        console.log(err);
-        reject(err);
-      } else {
-        resolve(same);
-      };
-    });
-  });
-};
 
 module.exports = mongoose.model("User", UserSchema);
