0b78bb02b1cb757f99eaa43f7350792459295d4d
Brad Cas <bmcasalvieri@gmail.com>
2019-06-01 00:40:07 -0400

    set up app.js and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import UserHome from './pages/UserHome';
+import AddUpdateEntry from './pages/AddUpdateEntry';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={UserHome} />
+          <Route exact path='/add' component={AddUpdateEntry} />
+          <Route exact path='/update/:id' component={AddUpdateEntry} />
+          <Route render={() => 404} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
