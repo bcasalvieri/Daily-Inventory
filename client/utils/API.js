de7b233d6187d4ad887f6c1ea3952e103b722008
Brad Cas <bmcasalvieri@gmail.com>
2019-06-01 00:19:35 -0400

    set up axios routes

---
+++ client/utils/API.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+// getUserProfile
+export const getUserProfile = () => {
+  return axios.get('/api/user')
+};
+
+// createEntry
+export const createEntry = (entryInfo) => {
+  return axios.post('/api/entry', entryInfo);
+};
+
+// getAllEntries
+export const getAllEntries = () => {
+  return axios.get('/api/entry');
+};
+
+// getEntryById
+export const getEntryById = (entryId) => {
+  return axios.get(`/api/entry/${entryId}`);
+};
+
+// updateEntry,
+export const updateEntry = (entryId, entryInfo) => {
+  return axios.put(`/api/entry/${entryId}`, entryInfo);
+};
+
+// deleteEntry
+export const deleteEntry = (entryId) => {
+  return axios.delete(`/api/entry/${entryId}`);
+};
+
+export default {
+  getUserProfile,
+  createEntry,
+  getAllEntries,
+  getEntryById,
+  updateEntry,
+  deleteEntry
+};
