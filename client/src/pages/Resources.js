39c32cf8af354ee13ba521e2def03112eb73de7d
Brad Cas <bmcasalvieri@gmail.com>
2019-06-09 20:09:10 -0400

    updated media queries

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Resources() {
   return (
     <div>
-      <div className='wrapper'>
+      <div className='wrapper pt-5'>
         <h1>This is the resources page</h1>
       </div>
     </div>
