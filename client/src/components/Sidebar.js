63c12811902f706e4f6b3aa97578f514bbc05d59
Brad Cas <bmcasalvieri@gmail.com>
2019-06-10 01:00:01 -0400

    added dropdown menu, updated userhome with resources

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import UserContext from '../utils/UserContext';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown } from 'react-bootstrap';
 import '../assets/css/style.css';
+import DropdownToggle from 'react-bootstrap/DropdownToggle';
 
 
-function Sidebar() {
+function Sidebar(props) {
 
   const userContext = useContext(UserContext);
 
@@ -24,22 +25,24 @@ function Sidebar() {
           <i className='fas fa-plus mr-1'></i> New Inventory
         </Button>
       </NavLink>
-      <NavLink to='/home' className='sidebar-link'>
-        <Button
+      <Button
           block
           className='menu-button mb-3'
+          onClick={props.saved}
         >
           <i className='fas fa-glasses mr-1'></i> {userContext.firstName}'s Inventories
-        </Button>
-      </NavLink>
-      <NavLink to='/resources' className='sidebar-link'>
-        <Button
-          block
-          className='menu-button mb-3'
-        >
+      </Button>
+      <Dropdown alignRight>
+        <DropdownToggle block className='menu-button'>
           <i className='fas fa-book-open mr-1'></i> Resources
-        </Button>
-      </NavLink>
+        </DropdownToggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={props.steps}>12 Steps</Dropdown.Item>
+          <Dropdown.Item onClick={props.principles}>8 Principles</Dropdown.Item>
+          <Dropdown.Item onClick={props.prayer}>Serenity Prayer</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   )
 }
--- client/src/pages/UserHome.js
+++ client/src/pages/UserHome.js
@@ -1,37 +1,97 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col, Jumbotron } from 'react-bootstrap';
 import EntriesFlipCard from '../components/EntriesFlipCard';
 import Sidebar from '../components/Sidebar';
 import '../assets/css/style.css';
 import UserContext from '../utils/UserContext';
+import TwelveSteps from '../components/TwelveSteps';
+import EightPrinciples from '../components/EightPrinciples';
+import SerenityPrayer from '../components/SerenityPrayer';
 
-export default function UserHome() {
+class UserHome extends Component {
 
-  const userContext = useContext(UserContext);
+  state = {
+    isShowing: "Saved Inventories"
+  }
 
-  return (
-    <React.Fragment>
-      <div className='wrapper'>
-        <Jumbotron fluid className='user-jumbotron mb-0'>
-          <Container className='user-container'>
-            <div className='d-flex flex-column align-items-center'>
-              <h3 className='mb-5'>Step Ten</h3>
-              <h1 className='text-center'>Continued to take personal inventory and when we were wrong promptly admitted it.</h1>
-            </div>
+  handleSavedInventories = () => {
+    this.setState({
+      isShowing: 'Saved Inventories'
+    })
+  }
+
+  handleTwelveSteps = () => {
+    this.setState({
+      isShowing: 'Twelve Steps'
+    });
+  };
+
+  handleEightPrinciples = () => {
+    this.setState({
+      isShowing: 'Eight Principles'
+    });
+  };
+
+  handleSerenityPrayer = () => {
+    this.setState({
+      isShowing: 'Serenity Prayer'
+    });
+  };
+
+
+  render() {
+
+    const isShowing = this.state.isShowing;
+    let showing;
+    let header;
+
+    if (isShowing === "Saved Inventories") {
+      header = <h2 className='entries-header text-center mb-4'> {this.context.firstName}'s Saved Inventories</h2>;
+      showing = <EntriesFlipCard />;
+    } else if (isShowing === "Twelve Steps") {
+      header = <h2 className='resources-header text-center mb-4'>{isShowing}</h2>
+      showing = <TwelveSteps />
+    } else if (isShowing === "Eight Principles") {
+      header = <h2 className='resources-header text-center mb-4'>{isShowing}</h2>
+      showing = <EightPrinciples />
+    } else if (isShowing === "Serenity Prayer") {
+      header = <h2 className='resources-header text-center mb-4'>{isShowing}</h2>
+      showing = <SerenityPrayer />
+    }
+
+    return (
+      <React.Fragment>
+        <div className='wrapper'>
+          <Jumbotron fluid className='user-jumbotron mb-0'>
+            <Container className='user-container'>
+              <div className='d-flex flex-column align-items-center'>
+                <h3 className='mb-5'>Step Ten</h3>
+                <h1 className='text-center'>Continued to take personal inventory and when we were wrong promptly admitted it.</h1>
+              </div>
+            </Container>
+          </Jumbotron>
+          <Container className='py-5'>
+            <Row>
+              <Col md={4} lg={3}>
+                <Sidebar
+                  steps={this.handleTwelveSteps}
+                  principles={this.handleEightPrinciples}
+                  prayer={this.handleSerenityPrayer}
+                  saved={this.handleSavedInventories}
+                />
+              </Col>
+              <Col md={8} lg={9}>
+                {header}
+                {showing}
+              </Col>
+            </Row>
           </Container>
-        </Jumbotron>
-        <Container className='py-5'>
-          <Row>
-            <Col md={4} lg={3}>
-              <Sidebar />
-            </Col>
-            <Col md={8} lg={9}>
-              <h2 className='entries-header text-center mb-4'> {userContext.firstName}'s Saved Inventories</h2>
-                <EntriesFlipCard />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </React.Fragment>
-  )
+        </div>
+      </React.Fragment>
+    )
+  }
 }
+
+UserHome.contextType = UserContext;
+
+export default UserHome;
