87c748286c742731aef85c6ec1c90eda7fb309e0
Brad Cas <bmcasalvieri@gmail.com>
2019-06-13 22:41:50 -0400

    added transition to inventory cards

--- client/src/components/EntriesFlipCard.js
+++ client/src/components/EntriesFlipCard.js
@@ -4,6 +4,7 @@ import UserContext from '../utils/UserContext';
 import moment from 'moment';
 import { Button, Card, CardGroup, Col } from 'react-bootstrap';
 import '../assets/css/style.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 function EntriesFlipCard() {
 
@@ -16,47 +17,55 @@ function EntriesFlipCard() {
   return (
     <div>
       <CardGroup>
-      {
-        (userContext.entries.length > 0)
-          ? (userContext.entries.map((entry) => {
-            return(
-              <Col xs={10} md={6} lg={4} className='mb-4'>
-                <Card className='entries-card flip-card' key={userContext.entries._id}>
-                  <div className='flip-card-inner'>
-                    <div className='flip-card-front'>
-                      <Card.Body className='d-flex justify-content-center align-items-center'>
-                        <Card.Title>{moment(entry.created).format('MMM Do, YYYY')}</Card.Title>
-                      </Card.Body>
-                    </div>
-                    <div className='flip-card-back'>
-                      <Card.Body className='d-flex flex-column justify-content-center'>
-                        <NavLink to={`/update/${entry._id}`} className='sidebar-link mb-3'>
-                          <Button
-                            block
-                            className='edit-button'
-                          >
-                            <i className='fas fa-edit mr-1'></i> Edit
+        {
+          (userContext.entries.length > 0)
+            ? (userContext.entries.map((entry) => {
+              return (
+                <Col xs={10} md={6} lg={4} className='mb-4'>
+                  <ReactCSSTransitionGroup
+                    transitionName='fade'
+                    transitionEnterTimeout={800}
+                    transitionLeaveTimeout={500}
+                    transitionAppear
+                    transitionAppearTimeout={500}
+                  >
+                    <Card className='entries-card flip-card' key={userContext.entries._id}>
+                      <div className='flip-card-inner'>
+                        <div className='flip-card-front'>
+                          <Card.Body className='d-flex justify-content-center align-items-center'>
+                            <Card.Title>{moment(entry.created).format('MMM Do, YYYY')}</Card.Title>
+                          </Card.Body>
+                        </div>
+                        <div className='flip-card-back'>
+                          <Card.Body className='d-flex flex-column justify-content-center'>
+                            <NavLink to={`/update/${entry._id}`} className='sidebar-link mb-3'>
+                              <Button
+                                block
+                                className='edit-button'
+                              >
+                                <i className='fas fa-edit mr-1'></i> Edit
                           </Button>
-                        </NavLink>
+                            </NavLink>
 
-                        <Button
-                          block
-                          variant='danger'
-                          onClick={() => userContext.handleDeleteEntry(entry._id)}
-                        >
-                          <i className="fas fa-trash-alt mr-1"></i> Delete
+                            <Button
+                              block
+                              variant='danger'
+                              onClick={() => userContext.handleDeleteEntry(entry._id)}
+                            >
+                              <i className="fas fa-trash-alt mr-1"></i> Delete
                         </Button>
-                      </Card.Body>
-                    </div>
-                  </div>
-                </Card>
-              </Col>
-            )
-          }))
-          : <div style={{width: '100%'}} className='d-flex justify-content-center'>
+                          </Card.Body>
+                        </div>
+                      </div>
+                    </Card>
+                  </ReactCSSTransitionGroup>
+                </Col>
+              )
+            }))
+            : <div style={{ width: '100%' }} className='d-flex justify-content-center'>
               <h4>No inventories to display!</h4>
             </div>
-      }
+        }
       </CardGroup>
     </div>
   )
